68804093337d9c0ea2292c6b5634f036581de72b
francimaria <francimaria.santos123@gmail.com>
2020-02-17 15:10:08 -0300

    best fit slope

--- regression.py
+++ regression.py
@@ -6,9 +6,11 @@ from sklearn.model_selection import cross_validate, train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt 
 from matplotlib import style
+import pickle
 
 style.use('ggplot')
 
+#Database
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
@@ -37,8 +39,15 @@ y = np.array(df['label'])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
-clf = LinearRegression(n_jobs=10)                               #The number of jobs to use for the computation.
-clf.fit(X_train, y_train)
+# clf = LinearRegression(n_jobs=-1)                               #The number of jobs to use for the computation.
+# clf.fit(X_train, y_train)
+# with open('linearregression.pickle', 'wb') as f:
+#     pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle','rb')
+clf = pickle.load(pickle_in)
+
+
 accuracy = clf.score(X_test, y_test)
 
 forecast_set =  clf.predict(X_lately)
